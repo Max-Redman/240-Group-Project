c13bff4eabb05df1c9dfced794ee5f28b8c372a1
YonatanAsfaw <yasfaw@umw.edu>
2023-04-18 13:47:20 -0400

    project

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("Masen was here");
-        System.out.println("Max was here!");
-        System.out.println("Yonatan was here");
+public class Move {
+    private String
 
-        System.out.println("Yay it works!!!");
-        System.out.println("Yay it really works!!!");
-
-    }
 }
